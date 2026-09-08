2c1a468a0f736b5c4e44e516af6df672075425c3
DONKotak <otiklipnice@seznam.cz>
2026-09-08 14:15:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Bump this whenever index.html/manifest/icons change, so old caches get
 // cleared and the phone picks up the new version next time it has signal.
 var CACHE_NAME = "kniha-jizd-v11";
-var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192-v2.png", "./icon-512-v2.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", function(event){
   event.waitUntil(
